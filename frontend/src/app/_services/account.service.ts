2c60e9a18c7ff1d6756807a275c333fc63704391
Gail Terman <gterman@gmail.com>
2020-11-13 17:28:17 -0500

    login works

--- frontend/src/app/_services/account.service.ts
+++ frontend/src/app/_services/account.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
-import { tap } from 'rxjs/operators'
+import { tap } from 'rxjs/operators';
 
 interface LoginRequest {
   id: string;
@@ -18,7 +18,7 @@ export class AccountService {
 
   // todo stop sending password in plain text
   login(id: string, password: string) {
-    return this.http.post(`${environment.backend}login`, {id, password}).pipe(
+    return this.http.post(`${environment.backend}api/login`, {id, password}).pipe(
       tap(d => console.log(d))
     );
   }
