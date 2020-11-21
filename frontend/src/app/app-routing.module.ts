28c89c3e01c80986917c0ddfc3f9685253456a31
Gail Terman <gterman@gmail.com>
2020-11-21 12:40:54 -0500

    add map routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { AuthGuard } from './_helpers';
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
 const scheduleModule = () => import('./schedule/schedule.module').then(x => x.ScheduleModule);
+const mapModule = () => import('./map/map.module').then(x => x.MapModule);
 
 const routes: Routes = [
   {path: '', component: LandingComponent, canActivate: [AuthGuard]},
   {path: 'account', loadChildren: accountModule},
-  {path: 'schedule', loadChildren: scheduleModule},
+  {path: 'schedule', loadChildren: scheduleModule, canActivate: [AuthGuard]},
+  {path: 'map', loadChildren: mapModule, canActivate: [AuthGuard]},
 
   //redirect home
   {path: '**', redirectTo: ''}
--- frontend/src/app/map/map-routing.module.ts
+++ frontend/src/app/map/map-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapComponent } from './map.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', component: MapComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
