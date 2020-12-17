ba844fef46afcd50699247c71b8690d950f4628f
Chris Lahey <clahey@clahey.net>
2020-12-17 15:48:22 -0500

    Update overlay size and caption position.
    Before this change, the caption was placed at an arbitrary height
    depending on the size of the image. It's now consistent from image to
    image.
    
    Once that position was fixed, it turns out the overlay was too small,
    so this makes it taller.

--- frontend/src/app/map/hall-costumes/hall-costumes.component.ts
+++ frontend/src/app/map/hall-costumes/hall-costumes.component.ts
@@ -35,8 +35,8 @@ export class HallCostumesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.images = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].map(
-      (n) => ({src: `https://picsum.photos/id/${n}/900/500`, caption: `Image number ${n}`}));
+    this.images = [{src: 'https://coolbackgrounds.io/images/backgrounds/white/pure-white-background-85a2a7fd.jpg', caption: 'This Image Is Totally White'}].concat([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].map(
+    (n) => ({src: `https://picsum.photos/id/${n}/900/500`, caption: `Image number ${n}`})));
     this.updateImageBufferSize();
     this.forceUpdateImageBuffer();
   }
