ee3bd1b5bf5ab33a6411f62748d7b3fa805bfc91
Gail Terman <gterman@gmail.com>
2021-01-01 21:50:59 -0500

    fix callout-box css inheritance

--- frontend/src/app/_components/callout-box/callout-box.component.ts
+++ frontend/src/app/_components/callout-box/callout-box.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-callout-box',
   templateUrl: './callout-box.component.html',
-  styleUrls: ['./callout-box.component.scss']
+  styleUrls: ['./callout-box.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class CalloutBoxComponent implements OnInit {
 
