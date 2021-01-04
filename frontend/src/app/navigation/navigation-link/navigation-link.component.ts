dd52d733755e6bd0f411e959d5e5ec35290aaa6a
Chris Lahey <clahey@clahey.net>
2021-01-04 02:03:14 -0500

    When logged out, switch the Log Out button to be a Log In button.
    Navigation now keeps track of the current link so that the Log In
    button can have the correct returnUrl.
    
    Also adds returnUrl support to navigation-link.

--- frontend/src/app/navigation/navigation-link/navigation-link.component.ts
+++ frontend/src/app/navigation/navigation-link/navigation-link.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class NavigationLinkComponent implements OnInit {
   @Input() link!: string;
   @Input() title!: string;
+  @Input() returnUrl = '';
 
   @Output() activated: EventEmitter<void> = new EventEmitter();
 
