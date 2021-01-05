fe14a2e452f63d0831c655b1c2324eb83c17e79f
Chris Lahey <clahey@clahey.net>
2021-01-04 22:07:05 -0500

    Add Door dragon to map links.
    Adds a directive for links that require the user to be logged in. It
    pops up a door dragon if the user isn't logged in. Requires a
    doorDragonStyle of either programming or social.
    
    The Door Dragon also supports styles of 'discord' and 'reset', but the
    links for those still go to the login screen, so they're not ready yet.

---
+++ frontend/src/app/_components/login-guarded-link.directive.spec.ts
@@ -0,0 +1,8 @@
+import { LoginGuardedLinkDirective } from './login-guarded-link.directive';
+
+describe('LoginGuardedLinkDirective', () => {
+  it('should create an instance', () => {
+    const directive = new LoginGuardedLinkDirective();
+    expect(directive).toBeTruthy();
+  });
+});
