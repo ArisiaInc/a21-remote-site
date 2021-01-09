ee6b75754797715acdd1f07e41695835505b503a
Mark "Justin du Coeur" Waks <github@waks.org>
2021-01-09 13:07:35 -0500

    Provide API entry point for adding an Arisian
    This doesn't actually *do* it yet, but it's the expected API so that Gail can try coding against it and confirm that it matches expectations.

--- arisia-remote/app/arisia/controllers/DiscordController.scala
+++ arisia-remote/app/arisia/controllers/DiscordController.scala
@@ -1,12 +1,13 @@
 package arisia.controllers
 
 import arisia.auth.LoginService
-import arisia.discord.{DiscordService, DiscordUserCredentials}
+import arisia.discord.{DiscordUserCredentials, DiscordService}
 import arisia.discord.DiscordUserCredentials
 import play.api.i18n.I18nSupport
+import play.api.libs.json.Json
 import play.api.mvc.{BaseController, ControllerComponents, EssentialAction}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{Future, ExecutionContext}
 
 class DiscordController(
   val controllerComponents: ControllerComponents,
@@ -25,9 +26,17 @@ class DiscordController(
     }
   }
 
-  def addMember(): EssentialAction = Action.async(controllerComponents.parsers.json[DiscordUserCredentials]) { implicit request =>
-    val creds = request.body
-    // TODO
-    ???
+  def addArisian(): EssentialAction = Action.async(controllerComponents.parsers.tolerantJson) { implicit request =>
+    val result = for {
+      json <- Some(request.body)
+      creds <- json.asOpt[DiscordUserCredentials]
+      // TODO: actually do the workflow. May need to make this level more sophisticated, for error handling
+    }
+      yield creds
+
+    result.map { _ =>
+      val msg = "Member added"
+      Future.successful(Ok(s"""{"success":"true", "message":"$msg"}"""))
+    }.getOrElse(Future.successful(BadRequest(s"""{"success":"false", "message":"That isn't the right input!"}""")))
   }
 }
