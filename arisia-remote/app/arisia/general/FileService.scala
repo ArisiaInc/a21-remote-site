a499a74d002124325875d1170a233c8188d4a221
Mark "Justin du Coeur" Waks <github@waks.org>
2021-01-10 17:38:24 -0500

    Actually store the Art Show metadata in the DB

--- arisia-remote/app/arisia/general/FileService.scala
+++ arisia-remote/app/arisia/general/FileService.scala
@@ -2,14 +2,31 @@ package arisia.general
 
 import arisia.db.DBService
 import play.api.Logging
+import doobie._
+import doobie.implicits._
 
-
+import scala.concurrent.{Future, ExecutionContext}
 
 trait FileService {
+  def setFile(tpe: FileType, content: String): Future[Int]
 }
 
 class FileServiceImpl(
   dbService: DBService
+)(
+  implicit ec: ExecutionContext
 ) extends FileService with Logging {
-
+  def setFile(tpe: FileType, content: String): Future[Int] = {
+    dbService.run(
+      sql"""
+           INSERT INTO text_files
+           (name, value)
+           VALUES
+           (${tpe.value}, $content)
+           ON CONFLICT (name)
+           DO UPDATE SET value = $content"""
+        .update
+        .run
+    )
+  }
 }
