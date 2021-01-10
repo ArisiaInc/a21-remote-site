ae7158d6c93230cec31b7056b1896ced0b456dea
Mark "Justin du Coeur" Waks <github@waks.org>
2021-01-10 13:06:52 -0500

    We're now actually loading the file as a String, as desired.

--- arisia-remote/app/arisia/controllers/FileController.scala
+++ arisia-remote/app/arisia/controllers/FileController.scala
@@ -1,16 +1,18 @@
 package arisia.controllers
 
+import akka.stream.scaladsl.Sink
 import akka.util.ByteString
 import arisia.auth.LoginService
 
 import scala.concurrent.ExecutionContext
 import play.api.i18n.I18nSupport
-import play.api.mvc.{BaseController, ControllerComponents, EssentialAction}
+import play.api.mvc.{ControllerComponents, BodyParser, BaseController, EssentialAction, MultipartFormData}
 import play.api.data._
 import play.api.data.Forms._
 import play.api.libs.streams.Accumulator
+import play.api.mvc.MultipartFormData.FilePart
 import play.core.Paths
-import play.mvc.Http.MultipartFormData.{FileInfo, FilePart}
+import play.core.parsers.Multipart.{FilePartHandler, FileInfo}
 
 class FileController(
   val controllerComponents: ControllerComponents,
@@ -25,10 +27,22 @@ class FileController(
     Ok(arisia.views.html.uploadMetadata())
   }
 
-  def uploadArtshowMetadata(): EssentialAction = Action(controllerComponents.parsers.raw) { implicit request =>
+  def byteStringFilePartHandler: FilePartHandler[ByteString] = {
+    case FileInfo(partName, filename, contentType, dispositionType) =>
+      Accumulator(Sink.fold[ByteString, ByteString](ByteString()) { (accumulator, data) =>
+        accumulator ++ data
+      }.mapMaterializedValue(fbs => fbs.map(bs => {
+        FilePart(partName, filename, contentType, bs)
+      })))
+  }
+
+  def multipartFormDataAsBytes: BodyParser[MultipartFormData[ByteString]] =
+    controllerComponents.parsers.multipartFormData(byteStringFilePartHandler)
+
+  def uploadArtshowMetadata(): EssentialAction = Action(multipartFormDataAsBytes) { implicit request =>
     // Rather than farting around with the terribly sophisticated and terribly hard-to-use multipart machinery
     // built into Play, we're doing this as dead-simply as we can:
-    val body = request.body.asBytes().map(_.utf8String)
+    val body: String = request.body.files.head.ref.utf8String
     println(s"The body is:\n$body")
     Redirect(routes.AdminController.home())
   }
