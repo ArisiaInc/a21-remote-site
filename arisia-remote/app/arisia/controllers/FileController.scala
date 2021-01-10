9eadc3296af64658a6f9774926db1cebbe411933
Mark "Justin du Coeur" Waks <github@waks.org>
2021-01-10 12:38:03 -0500

    Initial experiments
    This is now at the point where we are uploading the file, although we're not doing much with it.

--- arisia-remote/app/arisia/controllers/FileController.scala
+++ arisia-remote/app/arisia/controllers/FileController.scala
@@ -19,30 +19,17 @@ class FileController(
   implicit val ec: ExecutionContext
 ) extends BaseController
   with AdminControllerFuncs
-  with I18nSupport
-{
-  type FilePartHandler[A] = FileInfo => Accumulator[ByteString, FilePart[A]]
+  with I18nSupport {
 
-  def handleFilePartAsFile: FilePartHandler[File] = ???
-//  {
-//    case FileInfo(partName, filename, contentType, dispositionType) =>
-//      val perms       = java.util.EnumSet.of(OWNER_READ, OWNER_WRITE)
-//      val attr        = PosixFilePermissions.asFileAttribute(perms)
-//      val path        = JFiles.createTempFile("multipartBody", "tempFile", attr)
-//      val file        = path.toFile
-//      val fileSink    = FileIO.toPath(path)
-//      val accumulator = Accumulator(fileSink)
-//      accumulator.map {
-//        case IOResult(count, status) =>
-//          FilePart(partName, filename, contentType, file, count, dispositionType)
-//      }(ec)
-//  }
+  def manageMetadata(): EssentialAction = Action { implicit request =>
+    Ok(arisia.views.html.uploadMetadata())
+  }
 
-  def uploadCustom = Action(parse.multipartFormData(handleFilePartAsFile)) { request =>
-    val fileOption = request.body.file("name").map {
-      case FilePart(key, filename, contentType, file, fileSize, dispositionType) =>
-        file.toPath
-    }
-
-    Ok(s"File uploaded: $fileOption")
-  }}
+  def uploadArtshowMetadata(): EssentialAction = Action(controllerComponents.parsers.raw) { implicit request =>
+    // Rather than farting around with the terribly sophisticated and terribly hard-to-use multipart machinery
+    // built into Play, we're doing this as dead-simply as we can:
+    val body = request.body.asBytes().map(_.utf8String)
+    println(s"The body is:\n$body")
+    Redirect(routes.AdminController.home())
+  }
+}
