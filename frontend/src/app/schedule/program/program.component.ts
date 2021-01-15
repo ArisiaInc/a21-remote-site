2f3e66ca52cd8a2e5f2bbd168c274ac44d8d450c
Chris Lahey <clahey@clahey.net>
2021-01-14 19:25:49 -0500

    No, it is more important that changes be reflected than that we don't close in that case.

--- frontend/src/app/schedule/program/program.component.ts
+++ frontend/src/app/schedule/program/program.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ProgramFilter, ProgramItem } from '@app/_models';
 import { ScheduleService, StructuredEvents } from '@app/_services/schedule.service';
 import { Observable } from 'rxjs';
-import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-program',
@@ -16,11 +15,11 @@ export class ProgramComponent implements OnInit {
 
   // TODO allow filtering by url params
   ngOnInit(): void {
-    this.events$ = this.scheduleService.getSchedule().pipe(take(1));
+    this.events$ = this.scheduleService.getSchedule();
   }
 
   updateItems(filters: ProgramFilter) {
-    this.events$ = this.scheduleService.getSchedule(filters).pipe(take(1));
+    this.events$ = this.scheduleService.getSchedule(filters);
   }
 
 }
