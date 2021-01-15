f69e138413bee1a0462bc8350fc5d20b6fa19929
Chris Lahey <clahey@clahey.net>
2021-01-15 01:49:40 -0500

    Add Schedule sub-items to the drop down version of navigation.
    Also fixes the code to handle underlining of headers.

--- frontend/src/app/navigation/navigation-link/navigation-link.component.ts
+++ frontend/src/app/navigation/navigation-link/navigation-link.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-navigation-link',
     templateUrl: './navigation-link.component.html',
     styleUrls: ['./navigation-link.component.scss']
 })
-export class NavigationLinkComponent implements OnInit {
+export class NavigationLinkComponent implements OnChanges {
   @Input() link?: string = undefined;
   @Input() title!: string;
   @Input() returnUrl = '';
+  @Input() submenu = false
+  @Input() selectedKey = '';
+  @Input() id?: string;
+  selected = false;
 
   @Output() activated: EventEmitter<void> = new EventEmitter();
 
   constructor() {
   }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    if (!this.id) {
+      this.id = this.link;
+    }
+    if (this.selectedKey && this.id && this.selectedKey.startsWith(this.id)) {
+      this.selected = true;
+    } else {
+      this.selected = false;
+    }
     // assert (this.link !== undefined);
     // assert (this.title !== undefined);
   }
